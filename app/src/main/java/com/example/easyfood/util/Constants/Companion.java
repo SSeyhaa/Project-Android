2826077b76083f3bdf1ef1f1dc3aa1f0268e3f6b
Seyha SENG <seyha.seng@allweb.com.kh>
2023-12-08 17:59:42 +0700

    Easy Food Menu app

---
+++ app/src/main/java/com/example/easyfood/util/Constants/Companion.java
@@ -0,0 +1,13 @@
+package com.example.easyfood.util.Constants;
+
+public class Companion {
+
+    public static final String CATEGORY_NAME = "categoryName";
+    public static final String MEAL_ID = "mealId";
+    public static final String MEAL_STR = "mealStr";
+    public static final String MEAL_THUMB = "mealThumb";
+    public static final String MEAL_NAME = "mealName";
+    public static final String MEAL_AREA = "mealArea";
+
+
+}
